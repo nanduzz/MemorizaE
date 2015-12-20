172b4715949b81ac522c7b9a83a6ec090122ff4c
Fernando Carvalho <fernanduzz@gmail.com>
2015-12-20 17:33:39 -0200

    Corrigido posicionamento da câmera
    Corrigido posicionamento da câmera, para ficar no centro das cartas

--- Scripts/Controllers/GameGridController.cs
+++ Scripts/Controllers/GameGridController.cs
@@ -9,12 +9,16 @@ public class GameGridController : MonoBehaviour {
     [SerializeField]
     private GameObject cartaBase;
 
+    [SerializeField]
+    private GameObject Camera;
+
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
     void Awake()
     {
-        CriaGrid();
-
         
-
+        CriaGrid(spriteRenderer.bounds.size[0], spriteRenderer.bounds.size[1]);
     }
 
 	// Use this for initialization
@@ -27,9 +31,9 @@ public class GameGridController : MonoBehaviour {
 	
 	}
 
-    void CriaGrid()
+    void CriaGrid(float cartaX, float cartaY)
     {
-
+        GameObject carta = null;
         if ((tamanhoHorizontal * tamanhoVertical) % 2 != 0)
         {
             Debug.Log("tamanho do grid deve ser par");
@@ -38,9 +42,13 @@ public class GameGridController : MonoBehaviour {
         {
             for (int y = 0; y < tamanhoVertical; y++)
             {
-                GameObject carta = (GameObject)Instantiate(cartaBase, new Vector3(x, y), Quaternion.identity);
+                carta = (GameObject)Instantiate(cartaBase, new Vector3(x * cartaX, y * cartaY), Quaternion.identity);
                 carta.name = x + " - " + y;
             }
         }
+        Camera.transform.position = new Vector3(carta.transform.position.x / 2,
+                                                carta.transform.position.y / 2,
+                                                Camera.transform.position.z);
+
     }
 }
