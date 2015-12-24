c5f925d9e3d004f442da3cf7a8c102aeed2472ec
Fernando Carvalho <fernanduzz@gmail.com>
2015-12-24 08:47:57 -0200

    Criado rotina que embaralha as cartas
    Criado rotina que embaralha e posiciona as cartas, melhorado o código

--- Scripts/Controllers/GameGridController.cs
+++ Scripts/Controllers/GameGridController.cs
@@ -12,7 +12,7 @@ public class GameGridController : MonoBehaviour {
     private GameObject[] cartas;
 
     [SerializeField]
-    private GameObject Camera;
+    private GameObject cameraCena;
 
     [SerializeField]
     private SpriteRenderer spriteRenderer;
@@ -48,9 +48,41 @@ public class GameGridController : MonoBehaviour {
                 i++;
             }
         }
-        Camera.transform.position = new Vector3(cartas[i-1].transform.position.x / 2,
-                                                cartas[i-1].transform.position.y / 2,
-                                                Camera.transform.position.z);
+        PosicionaCamera();
+        EmbaralhaCartas(cartas);
+        PosicionaCartas(cartaX, cartaY);
+    }
 
+    void PosicionaCamera()
+    {
+        cameraCena.transform.position = new Vector3(cartas[cartas.Length - 1].transform.position.x / 2,
+                                                cartas[cartas.Length - 1].transform.position.y / 2,
+                                                cameraCena.transform.position.z);
+    }
+    void EmbaralhaCartas(GameObject[] list)
+    {
+        int n = list.Length;
+        while ( n> 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            GameObject aux = list[k];
+            list[k] = list[n];
+            list[n] = aux;
+        }
+        
+    }
+
+    void PosicionaCartas( float tamanhoCartaX, float tamanhoCartaY)
+    {
+        int i = 0;
+        for (int x = 0; x < tamanhoHorizontal; x++)
+        {
+            for (int y = 0; y < tamanhoVertical; y++)
+            {
+                cartas[i].transform.position = new Vector3(x * tamanhoCartaX, y * tamanhoCartaY);
+                i++;
+            }
+        }
     }
 }
